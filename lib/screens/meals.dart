95a88bc61ae0f590db7aa45169ca5d1f529063d6
Julian Currie <julian.currie@gmail.com>
2024-05-27 21:06:03 -0500

    added UI for a meal item image and data

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_meals/constants.dart';
 import 'package:flutter_meals/models/meal.dart';
+import 'package:flutter_meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -44,9 +45,7 @@ class MealsScreen extends StatelessWidget {
             backgroundColor: Colors.white,
             body: ListView.builder(
               itemCount: meals.length,
-              itemBuilder: (context, index) => Text(
-                meals[index].title,
-              ),
+              itemBuilder: (context, index) => MealItem(meal: meals[index]),
             ),
           );
   }
---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meals/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meal});
+
+  final Meal meal;
+
+  String capitalize(String s) => s[0].toUpperCase() + s.substring(1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      clipBehavior: Clip.hardEdge,
+      elevation: 10,
+      child: InkWell(
+        onTap: () {},
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(4),
+                topRight: Radius.circular(4),
+              ),
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(meal.imageUrl),
+                height: 240,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                width: 300,
+                color: Colors.black54,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 6,
+                  horizontal: 44,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealInfo(
+                          text: '${meal.duration} min',
+                          icon: Icons.schedule,
+                        ),
+                        const SizedBox(width: 16),
+                        MealInfo(
+                          text: capitalize(meal.complexity.name),
+                          icon: Icons.work,
+                        ),
+                        const SizedBox(width: 16),
+                        MealInfo(
+                          text: capitalize(meal.affordability.name),
+                          icon: Icons.attach_money,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MealInfo extends StatelessWidget {
+  const MealInfo({super.key, required this.text, required this.icon});
+
+  final String text;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          size: 16,
+          color: Colors.white,
+        ),
+        const SizedBox(width: 8),
+        Text(
+          text,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ],
+    );
+  }
+}
